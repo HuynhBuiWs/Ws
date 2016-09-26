cbc86ca7a69938eb4b040a6e9ab4fbe110dc55e7
HuynhBuiWs <buihuynh96@gmail.com>
2016-09-26 09:22:44 +0700

    dasdas

---
+++ Cs1/Cs1/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cs1
+{
+    class Class1<T>
+    {
+        private T data;
+
+        public T Data
+        {
+            get { return data; }
+            set { data = value; }
+        }
+        Class1<T> next;
+    
+
+internal Class1<T> Next
+{
+  get { return next; }
+  set { next = value; }
+}}
+}
