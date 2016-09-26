049b19bb737ca7c78c35a839cda7303212e2067b
HuynhBuiWs <buihuynh96@gmail.com>
2016-09-26 09:28:44 +0700

    dfsadff

--- Cs1/Cs1/List.cs
+++ Cs1/Cs1/List.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace Cs1
 {
-    class List
+    class List<T>
     {
+        Class1<T> head;
+
+        internal Class1<T> Head
+        {
+            get { return head; }
+            set { head = value; }
+        }
+        public List() { }
+        public void addfirst(Class1<T> e) { e.Next = head; this.head = e; }
     }
 }
